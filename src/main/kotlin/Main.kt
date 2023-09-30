de577f36a58bedf25939d4329beaafc0f28fb71a
karina <135805670+karinakravets@users.noreply.github.com>
2023-09-30 13:40:56 +0300

    функция работает

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,16 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println("Число в двоичной системе: " + tretiya(readln().toInt()))
+}
+fun tretiya(a: Int): Int {
+    var a = a
+    var b = 0
+    var c = 1
+    var d: Int
+    while (a > 0) {
+        d = a % 2
+        a /= 2
+        b+= d * c
+        c *= 10
+    }
+    return b
 }
