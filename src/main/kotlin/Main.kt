0354bce369f843fca8b1965d98d25046d403200f
karina <135805670+karinakravets@users.noreply.github.com>
2023-10-28 13:18:31 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,2 +1,16 @@
 fun main() {
-    println("Число в десятичной системе: ${readln().toString(2)}")
+    println("Число в двоичной системе: " + tretiya(readln().toInt()))
+}
+fun tretiya(a: Int): Int {
+    var a = a
+    var b = 0
+    var c = 1
+    var d: Int
+    while (a > 0) {
+        d = a % 2
+        a /= 2
+        b+= d * c
+        c *= 10
+    }
+    return b
+}
